1b1b6a5f79f80aa257219d4dcf5e48fa1ac023ed
Sajjad Taghinezhad <mail@sajx.net>
2025-02-12 19:02:20 +0330

    change presentation page to insert

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Portfolio from "../components/Portfolio"
+import Presentation from "@/components/Presentation"
 
-export default function Home() {
+export default function PresentationPage() {
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-8 text-center">My Interactive Portfolio</h1>
-      <Portfolio />
+    <main className="container mx-auto p-4 h-screen flex flex-col">
+      <h1 className="text-4xl font-bold mb-8 text-center">My Portfolio Presentation</h1>
+      <Presentation />
     </main>
   )
 }
