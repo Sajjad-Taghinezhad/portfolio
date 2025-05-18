a50d147b958f855d2cc6f75199590805abaaf91b
Sajjad Taghinezhad <mail@sajx.net>
2025-05-18 20:30:20 +0330

    Update upload directory in insert handler to store files in the public directory

--- pages/api/insert.ts
+++ pages/api/insert.ts
@@ -37,7 +37,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const form = formidable({
         multiples: false, // Only allow a single file upload
-        uploadDir: path.join(process.cwd(), 'public/uploads'), // Directory to store uploaded files
+        uploadDir: path.join(process.cwd(), 'public'), // Directory to store uploaded files
         keepExtensions: true, // Keep file extensions
         filter: (part) => {
             // Only accept image and PDF files
